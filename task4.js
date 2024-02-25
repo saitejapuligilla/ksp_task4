f31dd42cef3ff1b7f6e6e46deb81ea1eeb54a6c4
sai teja puligilla <160746186+saitejapuligilla@users.noreply.github.com>
2024-02-25 23:16:44 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,25 @@
+//Basics of JavaScript
+alert("Welcome to my page:");
+console.log("Thanks for visiting");
+
+let num1=5;
+let num2=10;
+let num3=14;
+
+//prints the value in variable num3
+console.log(num3);
+//prints as num1
+console.log("num1")
+
+//let us understand some datatypes
+let num4=6;
+let fnumber1=7.2;
+let string1="sai teja";
+let char1="P";
+let bool=true;
+
+console.log(typeof(num4));
+console.log(typeof(fnumber1));
+console.log(typeof(string1));
+console.log(typeof(char1));
+console.log(typeof(bool));
